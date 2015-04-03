a6963e00a7f006dbd41efa5da7b1e87a71c30a5c
Svetozar Miucin <smiucin@sfu.ca>
2015-04-02 17:27:33 -0700

    i3status configs

--- i3setup.sh
+++ i3setup.sh
@@ -5,4 +5,5 @@ gsettings set org.nemo.desktop show-desktop-icons false
 mkdir ~/.i3
 rm ~/.i3/config
 ln -s `pwd`/config ~/.i3/config
+ln -s `pwd`/i3status.conf ~/.i3status.conf
 cp toggle-mute ~/.i3/
