4749fbe385f4dddd690fe239a2f7e19381c51dce
Svetozar Miucin <smiucin@sfu.ca>
2015-04-03 14:13:16 -0700

    added volume icon and fontawesome

--- i3setup.sh
+++ i3setup.sh
@@ -2,6 +2,8 @@
 sudo apt-get install feh i3status i3lock
 gsettings set org.nemo.desktop show-desktop-icons false
 
+sudo unzip fonts/font-awesome-4.3.0.zip -d /usr/share/fonts 
+
 mkdir ~/.i3
 rm ~/.i3/config
 ln -s `pwd`/config ~/.i3/config
